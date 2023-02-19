e6b52f8344b363ceaa88e5cb409f3c7d565dac00
Raul Vera <110500250+Ralusagi@users.noreply.github.com>
2023-02-19 13:58:54 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function mostrar(){
+    alert("Lamento que esté mal decorado");
+    alert("A pesar de todo fue hecho con amor");
+}
+
+function mostrar1(){
+    alert("Te amo");
+}
